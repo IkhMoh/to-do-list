03c60b3a58cd83201105a82fc23a29457a2eded5
yasser ramdani <139624089+IkhMoh@users.noreply.github.com>
2025-03-13 03:17:28 +0100

    add log

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ const App = () => {
   const [checkbox, setCheckbox] = useState([]);
   function alerttaxt() {
     setExit(!exit);
-    
   }
   // function Checkboxset() {
   //   // setTaskvalue([{...taskvalue, checkbox: !taskvalue.checkbox }]);
